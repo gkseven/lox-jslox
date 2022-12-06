08cb07c62f2676fca57cf79ad73087146ebd3817
xhsgkn <xhsgkn@gmail.com>
2022-12-07 02:47:06 +0300

    Read from file and from stdin

--- src/run.ts
+++ src/run.ts
@@ -1,21 +1,34 @@
 /*READ EVAL PRINT LOOP*/
 
 import * as readline from 'node:readline';
+import { readFileSync } from 'node:fs';
 
+function run(source: string): void {
+  console.log(source);
+}
 
-
-export function runFile(file: string): void {
-  console.log(`Running file: ${file}`);
+export function runFile(filepath: string): void {
+  try {
+    const fileContents = readFileSync(filepath);
+    run(fileContents.toString());
+  }
+  catch (error) {
+    const errMsg = (error as Error).message;
+    console.error(errMsg);
+  }
 }
 
 export function runPrompt(): void {
-  const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
   rl.setPrompt('>> ');
 
   rl.prompt();
   rl.on('line', (line) => {
-    console.log(line);
+    run(line);
     rl.prompt();
   });
-
 }
+
