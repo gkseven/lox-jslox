a164033d6a9645f3dd7cf6806080243dff2f013a
dusean435 <dusean435@gmail.com>
2022-12-08 01:31:02 +0300

    Make Lox more static

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import Lox from './Lox';
 
-const interpreter = new Lox();
 
 if (process.argv.length > 3) {
   console.log(`Usage: ${process.argv[1]} [script]}`);
   process.exit(64);
 } else if (process.argv.length == 3) {
-  interpreter.runFile(process.argv[2]);
+  Lox.runFile(process.argv[2]);
 } else {
-  interpreter.runPrompt();
+  Lox.runPrompt();
 }
