1c8834e5d1c9481fde0c6b88cedfa7f14e47b6f7
ksnine <mail.ksnine@gmail.com>
2022-12-08 09:11:12 +0300

    Add scanner class

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,4 +1,213 @@
-/*
-The scanner takes in raw source code and returns a list of tokens.
-*/
+import Lox from './Lox';
+import TokenType from './TokenType';
 
+class Scanner {
+  private readonly source: string;
+  private readonly tokens: Token[] = new Array();
+  private static readonly keywords: Map<string, TokenType> = new Map([]);
+  private start: number = 0;
+  private current: number = 0;
+  private line: number = 1;
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  static {
+    this.keywords.set('and', TokenType.AND);
+    this.keywords.set('class', TokenType.CLASS);
+    this.keywords.set('else', TokenType.ELSE);
+    this.keywords.set('false', TokenType.FALSE);
+    this.keywords.set('for', TokenType.FOR);
+    this.keywords.set('fun', TokenType.FUN);
+    this.keywords.set('if', TokenType.IF);
+    this.keywords.set('nil', TokenType.NIL);
+    this.keywords.set('or', TokenType.OR);
+    this.keywords.set('print', TokenType.PRINT);
+    this.keywords.set('return', TokenType.RETURN);
+    this.keywords.set('super', TokenType.SUPER);
+    this.keywords.set('this', TokenType.THIS);
+    this.keywords.set('true', TokenType.TRUE);
+    this.keywords.set('var', TokenType.VAR);
+    this.keywords.set('while', TokenType.WHILE);
+  }
+
+  scanTokens(): Token[] {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    const eof = new Token(TokenType.EOF, '', null, this.line);
+    this.tokens.push(eof);
+
+    return this.tokens;
+  }
+
+  private isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
+
+  private scanToken(): void {
+    const c = this.advance();
+    switch (c) {
+      case '(':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ')':
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case '{':
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case '}':
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case ',':
+        this.addToken(TokenType.COMMA);
+        break;
+      case '.':
+        this.addToken(TokenType.DOT);
+        break;
+      case '-':
+        this.addToken(TokenType.MINUS);
+        break;
+      case '+':
+        this.addToken(TokenType.PLUS);
+        break;
+      case ';':
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case '*':
+        this.addToken(TokenType.STAR);
+        break;
+      case '!':
+        this.addToken(this.match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        this.addToken(
+          this.match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL
+        );
+        break;
+      case '<':
+        this.addToken(this.match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        this.addToken(
+          this.match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER
+        );
+        break;
+      case '/':
+        if (this.match('/')) {
+          while (this.peek() != '\n' && !this.isAtEnd()) this.advance();
+        } else {
+          this.addToken(TokenType.SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace.
+        break;
+      case '\n':
+        this.line++;
+        break;
+      case '"':
+        this.string();
+        break;
+      default:
+        if (this.isDigit(c)) {
+          this.number();
+        } else if (this.isAlpha(c)) {
+          this.identifier();
+        } else {
+          Lox.reportError(this.line, 'Unexpected character.');
+        }
+        break;
+  }
+}
+  private string(): void {
+    while (this.peek() != '"' && !this.isAtEnd()) {
+      if (this.peek() == '\n') this.line++;
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      Lox.reportError(this.line, 'Unterminated string.');
+      return;
+    }
+
+    this.advance();
+    const value = this.source.substring(this.start + 1, this.current - 1);
+    this.addToken(TokenType.STRING, value);
+  }
+
+  private isDigit(c: string): boolean {
+    return c >= '0' && c <= '9';
+  }
+
+  private number(): void {
+    while (this.isDigit(this.peek())) this.advance();
+
+    if (this.peek() == '.' && this.isDigit(this.peekNext())) {
+      this.advance();
+      while (this.isDigit(this.peek())) this.advance();
+    }
+
+    this.addToken(
+      TokenType.NUMBER,
+      parseFloat(this.source.substring(this.start, this.current))
+    );
+  }
+
+  private identifier():void {
+    while (this.isAlphaNumeric(this.peek())) this.advance();
+
+    const text = this.source.substring(this.start, this.current);
+    const type = Scanner.keywords.get(text) || TokenType.IDENTIFIER;
+    this.addToken(type);
+  }
+
+  private isAlpha(c:string): boolean {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private isAlphaNumeric(c:string): boolean {
+    return this.isAlpha(c) || this.isDigit(c);
+  }
+
+  private match(expected: string): boolean {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) !== expected) return false;
+
+    this.current++;
+    return true;
+  }
+
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private peek(): string {
+    if (this.isAtEnd()) return '\0';
+    return this.source.charAt(this.current);
+  }
+
+  private peekNext(): string {
+    if (this.current + 1 >= this.source.length) return '\0';
+    return this.source.charAt(this.current + 1);
+  }
+
+  private addToken(
+    type: TokenType,
+    literal?: string | number | null | undefined
+  ): void {
+    const text = this.source.substring(this.start, this.current);
+    const token = new Token(type, text, literal, this.line);
+    this.tokens.push(token);
+  }
+}
+
+export default Scanner;
