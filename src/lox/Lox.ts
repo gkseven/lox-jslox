37fad8c7293231fb61cc2866ea673de8b4cb9503
xhsgkn <xhsgkn@gmail.com>
2023-01-09 03:06:41 +0300

    Added some comments

--- src/lox/Lox.ts
+++ src/lox/Lox.ts
@@ -2,9 +2,19 @@ import * as readline from 'node:readline';
 import { readFileSync } from 'node:fs';
 import Scanner from '../scanner/Scanner';
 
+/**
+ * The Lox class is the entry point for the Lox interpreter.
+ * It is responsible for reading the source code and passing it to the Scanner.
+ * It also handles the command line arguments and decides whether to run the REPL or a file.
+ */
 class Lox {
-  private static hadError: boolean = false;
+  private static hadError: boolean = false; // Flag to track if an error has occurred.
 
+  /**
+   * Executes the program with the given file path.
+   * @param filepath The path to the file to be executed.
+   * @returns void
+   */
   static runFile(filepath: string): void {
     try {
       const fileContents = readFileSync(filepath);
@@ -16,6 +26,10 @@ class Lox {
     }
   }
 
+  /**
+   * Runs the REPL.
+   * @returns void
+   */
   static runPrompt(): void {
     const rl = readline.createInterface({
       input: process.stdin,
@@ -31,21 +45,27 @@ class Lox {
     });
   }
 
+  /**
+   * Runs the scanner on the given source code.
+   * @param source The source code to be scanned.
+   * @returns void
+   */
   private static run(source: string): void {
     const scanner = new Scanner(source);
     const tokens = scanner.scanTokens();
 
-<<<<<<< HEAD
     tokens.forEach((token) => {
       console.log(token.toString());
     });
-=======
-    for (const token in tokens) {
-      console.log(token);
-    }
->>>>>>> 84be6071ef09b6cda4b6910338a4cf9d4383b4f4
   }
 
+  /**
+   * Reports an error to the console.
+   * @param line The line number where the error occurred.
+   * @param where The location where the error occurred.
+   * @param message The error message.
+   * @returns void
+   */
   static reportError(line: number, where?: string, message?: string) {
     console.error(`[line: ${line} Error ${where} : ${message}`);
     this.hadError = true;
